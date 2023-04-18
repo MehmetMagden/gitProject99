9fc346b3a8c43256fc27cb1621c4ae032653b760
Mehmet Maden <yasamtr@gmail.com>
2023-04-18 18:14:38 +0300

    Mehmet V2

---
+++ src/CommonClass.java
@@ -0,0 +1,27 @@
+public class CommonClass {
+    public static void main(String[] args) {
+
+        // Mehmet
+
+        System.out.println("Here is for mehmet");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Hasan
+
+    }
+}
